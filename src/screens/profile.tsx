e78a7c513b02249ae1675f40919ed6ecb22b039d
SUYASH VASHISHTHA <suyashvashishtha@gmail.com>
2021-09-30 21:05:04 +0530

    hide delete acc

--- src/screens/profile.tsx
+++ src/screens/profile.tsx
@@ -39,13 +39,20 @@ export default function ProfileScreen() {
             .catch((error: any) => { alert(error.message) })
     }
 
-    const deleteAccount = () => {
-        deleteUser(user).then(() => {
-            dispatch(setUserLogOutState())
-            alert("Your Account has been Deleted")
-            history.push({ pathname: "/" })
-        }).catch((error) => alert(error.message));
-    }
+
+    // const deleteAccount = () => {
+    //     let delConfirm = prompt("Are your you want to delete this account?\n This Action cant be undone \nPlease enter your registered email to confirm:", "Your email")
+    //     if (delConfirm === null || delConfirm !== `${userEmail}`) {
+    //         return
+    //     } else {
+    //         deleteUser(user).then(() => {
+    //             dispatch(setUserLogOutState())
+    //             alert("Your Account has been Deleted")
+    //             history.push({ pathname: "/" })
+    //         }).catch((error) => alert(error.message));
+    //     }
+
+    // }
 
     const logout = () => {
         signOut(auth).then(() => {
@@ -67,7 +74,7 @@ export default function ProfileScreen() {
                         <span className="email">{userDetails[0].email}</span>
                         <div className="base-flex">
                             <PrimaryButton onClick={forgetPassword} title={"Forget Password"} size={'sm'} />
-                            <PrimaryButton onClick={deleteAccount} title={"Delete Account"} size={'sm'} />
+                            {/* <PrimaryButton onClick={deleteAccount} title={"Delete Account"} size={'sm'} /> */}
                         </div>
                         <PrimaryButton onClick={logout} title={"Log Out"} color={"white"} size={'sm'}>
                             <BiLogOut size={20} />
